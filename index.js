ba5c8af1a2dd6de3c96c8a4c41638432369a2bf2
muhiminul-islam <islammuhiminul11@gmail.com>
2023-05-12 18:35:37 +0600

    post method integrate with mongodb and send api in client side

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -27,10 +27,30 @@ async function run() {
         await client.connect();
 
         const serviceCollection = client.db('carDoctor').collection('services');
+        const bookingCollection = client.db('carDoctor').collection('bookings');
 
         app.get('/services', async(req, res) => {
             const cursor = serviceCollection.find();
             const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.get('/services/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+
+            const options = {
+                // Include only the `title` and `imdb` fields in the returned document
+                projection: { title: 1, price: 1, service_id: 1 },
+              };
+
+            const result = await serviceCollection.findOne(query, options);
+            res.send(result);
+        })
+
+        app.post('/bookings', async(req, res) => {
+            const booking = req.body;
+            const result = await bookingCollection.insertOne(booking);
             res.send(result);
         })
 
