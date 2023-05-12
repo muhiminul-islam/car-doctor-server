aaa0719dfc5caf18a877bc2460e714fc4c880e08
muhiminul-islam <islammuhiminul11@gmail.com>
2023-05-12 20:53:32 +0600

    upto updated booking route create

--- index.js
+++ index.js
@@ -35,13 +35,23 @@ async function run() {
             res.send(result);
         })
 
+        app.get('/bookings', async(req, res) =>{
+            console.log(req.query.email);
+            let query = {};
+            if(req.query?.email){
+                query = {email: req.query.email }
+            }
+            const result = await bookingCollection.find(query).toArray();
+            res.send(result);
+        })
+
         app.get('/services/:id', async(req, res) => {
             const id = req.params.id;
             const query = {_id: new ObjectId(id)}
 
             const options = {
                 // Include only the `title` and `imdb` fields in the returned document
-                projection: { title: 1, price: 1, service_id: 1 },
+                projection: { title: 1, price: 1, service_id: 1, img: 1 },
               };
 
             const result = await serviceCollection.findOne(query, options);
@@ -54,6 +64,16 @@ async function run() {
             res.send(result);
         })
 
+        app.put('/bookings/:id', async(req, res) => {
+            const updatedBooking = req.body;
+        })
+
+        app.delete('/bookings/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)}
+            const result = await bookingCollection.deleteOne(query);
+            res.send(result);
+        })
 
 
         // Send a ping to confirm a successful connection
